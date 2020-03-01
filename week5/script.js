f57133428857957e5a6d613cd2ffa0e81e1bc12d
Brendan Murphy <bjohnmurphy1984@gmail.com>
2020-03-01 15:07:44 -0500

    week 5

--- week5/script.js
+++ week5/script.js
@@ -1,8 +1,13 @@
     let app =  new Vue ({
         el: '#app',
         data: {
-            items: [],
-            item: null,
-            testArray: ['a','b','c']
+            randomNumber1: Math.floor((Math.random() *10) + 1),
+            randomNumber2: Math.floor((Math.random() *10) + 1),
+            answer: '',
+        },
+        methods: {
+            submitanswer: function () {
+                console.log('Submit answer...');
+            }
         }
     })
