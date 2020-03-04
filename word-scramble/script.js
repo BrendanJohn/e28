5626e228f3e562f5f9afba15e57ec3222f2c0928
n0282584 <Brendanj.Murphy@libertymutual.com>
2020-03-04 15:48:40 -0500

    word scramble work

--- word-scramble/script.js
+++ word-scramble/script.js
@@ -1,8 +1,48 @@
-    let app =  new Vue ({
-        el: '#app',
-        data: {
-            items: [],
-            item: null,
-            testArray: ['a','b','c']
+let app =  new Vue ({
+    el: '#app',
+    data: {
+        mysteryWord: 'someWord',
+        guess: '',
+        answer: 'someWord',
+        showHint: true,
+        playerName: '',
+        hint: 'hint for word',
+        correct: false,
+        feedback: '',
+        showGame: false,
+        showResult: false
+    },
+    methods: {
+        submitAnswer: function () {
+            this.showResult = true;
+            if (this.answer == this.correctAnswer) {
+                this.feedback = 'You got it! Nice work.'
+                this.correct = true
+            } else {
+                this.imageSource = 'images/sad.jpg'
+                this.feedback = 'Sorry, thats not correct. Please try again.'
+                this.correct = false
+            }
+        },
+        submitNameAndPlay: function () {
+            this.showGame = true;
+        },
+
+        reset: function () {
+            this.showGame = false;
+            this.showResult = false;
+            this.guess = '';
+            this.playerName = '';
+            this.feedback = '';
+            this.correct = false;
         }
-    })
+    },
+    mounted: function () {
+        console.log('The app has been mounted');
+    },
+    computed: {
+        correctAnswer: function () {
+            return this.answer;
+        }
+    }
+})
