9b87e3e5d259104c1b416c15eae242d9bcaf2bab
n0282584 <Brendanj.Murphy@libertymutual.com>
2020-03-05 10:05:30 -0500

    word scramble updates

--- word-scramble/script.js
+++ word-scramble/script.js
@@ -11,11 +11,21 @@ let app =  new Vue ({
         showGame: false,
         showResult: false,
         lastWord: '',
-        words: [
+        level: 'easy',
+        easyWords: [
             ['apple', 'Sometimes red, sometimes delicious'],
             ['washington', 'Rushmore’s left edge'],
             ['pumpkin', 'Halloween’s favorite fruit'],
             ['football', 'Play with your hands or feet, depending on your location']
+        ],
+        mediumWords: [
+            ['license', 'You might have one in your wallet'],
+            ['restaurant', 'A nice place to grab a bite to eat'],
+            ['recommendation', 'Helps you get the job interview']
+        ],
+        hardWords: [
+            ['brontosaurus', 'Herbivore from a long time ago'],
+            ['massachusetts', 'Home of beantown']
         ]
     },
     methods: {
@@ -30,10 +40,10 @@ let app =  new Vue ({
             }
         },
         submitNameAndPlay: function () {
-            this.mysteryWord = this.getRandomWord();
+            this.mysteryWord = this.getRandomWord(this.level);
             this.showGame = true;
             this.shuffledWord = this.shuffleMysteryWord;
-            this.hint = this.words.find(obj => obj[0] == this.mysteryWord)[1];  
+            this.hint = this.getHint(this.level);  
         },
         reset: function () {
             this.showGame = 'false';
@@ -44,8 +54,27 @@ let app =  new Vue ({
             this.correct = false;
             this.lastWord = this.mysteryWord;
         },
-        getRandomWord: function () {
-            return this.words[Math.floor(Math.random() * this.words.length)][0]
+        getRandomWord: function (level) {
+            if (level == 'easy') {
+                return this.easyWords[Math.floor(Math.random() * this.easyWords.length)][0]
+            }
+            else if ( level == 'medium'){
+                return this.mediumWords[Math.floor(Math.random() * this.mediumWords.length)][0]
+            }
+            else {
+                return this.hardWords[Math.floor(Math.random() * this.hardWords.length)][0]
+            }
+        },
+        getHint: function (level) {
+            if (level == 'easy') {
+                return this.easyWords.find(obj => obj[0] == this.mysteryWord)[1]; 
+            }
+            else if ( level == 'medium'){
+                return this.mediumWords.find(obj => obj[0] == this.mysteryWord)[1]; 
+            }
+            else {
+                return this.hardWords.find(obj => obj[0] == this.mysteryWord)[1]; 
+            }         
         }
     },
     mounted: function () {
