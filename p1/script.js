c2326255d0795c2397e88dfad91785aa7b0e17f3
Brendan M <bjohnmurphy1984@gmail.com>
2020-03-19 16:47:35 -0400

    moved game mechanics to vue

--- p1/script.js
+++ p1/script.js
@@ -4,14 +4,16 @@
             playerSelection: '',
             computerSelection: '',
             choices: ['rock', 'paper', 'scissors'],
+            outcome: '',
+            gameCompleted: false,
             winner: ''
         },
         methods: {
             submitSelection: function () {
                 if (this.playerSelection) {  
-                    console.log(this.playerSelection + ' vs ' + this.computerSelection);
+                    this.gameCompleted = false;
+                    this.computerSelection = this.choices[Math.floor(Math.random()*this.choices.length)];
                     this.rockPaperScissors(this.playerSelection, this.computerSelection);
-                    console.log('winner: ' + this.winner);
                 }
                 else {
                     console.log('please make a selection');		
@@ -24,38 +26,47 @@
             //determine the outcome of the game
             rockPaperScissors: function(playerSelection, computerSelection) {
                 if (playerSelection == computerSelection) {
+                    this.outcome = 'tie';
                     this.winner = 'tie';
                 }
                 else if (playerSelection == 'rock') {
                     if (computerSelection == 'scissors') {
-                        this.winner = playerSelection;
+                        this.outcome = playerSelection;
+                        this.winner = 'player';
                     }
                     else {
-                        this.winner = computerSelection;
+                        this.outcome = computerSelection;
+                        this.winner = 'computer';
                     }
                 }
                 else if (playerSelection == 'paper') {
                     if (computerSelection == 'rock') {
-                        this.winner = playerSelection;
+                        this.outcome = playerSelection;
+                        this.winner = 'player';
                     }
                     else {
-                        this.winner = computerSelection;
+                        this.outcome = computerSelection;
+                        this.winner = 'computer';
                     }
                 }
                 else if (playerSelection == 'scissors') {
                     if (computerSelection == 'paper') {
-                        this.winner = playerSelection;
+                        this.outcome = playerSelection;
+                        this.winner = 'player';
                     }
                     else {
-                        this.winner = computerSelection;
+                        this.outcome = computerSelection;
+                        this.winner = 'computer';
                     }
                 }
+                this.gameCompleted = true;
             }
         },
         watch: {
             // When playerSelection changes, this function is invoked
             playerSelection: function () {
                 this.computerSelection = this.choices[Math.floor(Math.random()*this.choices.length)];
+                this.gameCompleted = false;
             }
         }
     })
