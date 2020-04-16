255ca5749ab816bfa83244d4d6f60948ad1531bb
Brendan M <bjohnmurphy1984@gmail.com>
2020-04-16 16:20:14 -0400

    zipfoods updates for admin page

--- zipfoods/src/main.js
+++ zipfoods/src/main.js
@@ -3,6 +3,7 @@ import App from '@/App.vue'
 import VueRouter from 'vue-router'
 import HomePage from '@/components/pages/HomePage.vue'
 import ProductPage from '@/components/pages/ProductPage.vue'
+import AdminPage from '@/components/pages/AdminPage.vue'
 import ProductsPage from '@/components/pages/ProductsPage.vue'
 import CategoriesPage from '@/components/pages/CategoriesPage.vue'
 import ProductCreatePage from '@/components/pages/ProductCreatePage.vue'
@@ -14,6 +15,7 @@ Vue.config.productionTip = false
 //names routes are a good way to remove redundancy
 const routes = [
   {path: '/', component: HomePage, name: 'home'},
+  {path: '/admin', component: AdminPage, name: 'admin'},
   {path: '/products', component: ProductsPage, name: 'products'},
   {path: '/product/:slug', component: ProductPage, name: 'product', props: true},
   {path: '/categories', component: CategoriesPage, name: 'categories'},
